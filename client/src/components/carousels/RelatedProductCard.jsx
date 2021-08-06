347701ef22abfc88daabed41e006a06cffbb1163
alan <alangu.ua@gmail.com>
2021-08-06 10:59:38 -0400

    Building Modal

--- client/src/components/carousels/RelatedProductCard.jsx
+++ client/src/components/carousels/RelatedProductCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import Modal from './Modal.jsx'
 import ReactStars from "react-rating-stars-component";
 import { TOKEN } from './../../config.js';
 import { FaRegStar } from "react-icons/fa";
@@ -16,52 +17,52 @@ class RelatedProductCard extends React.Component {
   }
   componentDidMount() {
     if (this.state.productId) {
-    let options = {
-      type: 'get',
-      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/products/${this.props.productID}`,
-      headers: {
-        Authorization: TOKEN
-      },
-      params: {
-        product_id: this.props.productID
-      }
-    };
-    axios(options)
-      .then((res) => {
-        var buildItem = {
-          id: res.data.id,
-          name: res.data.name,
-          category: res.data.category,
-          price: res.data.default_price
-        };
+      let options = {
+        type: 'get',
+        url: `https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/products/${this.props.productID}`,
+        headers: {
+          Authorization: TOKEN
+        },
+        params: {
+          product_id: this.props.productID
+        }
+      };
+      axios(options)
+        .then((res) => {
+          var buildItem = {
+            id: res.data.id,
+            name: res.data.name,
+            category: res.data.category,
+            price: res.data.default_price
+          };
 
-        options.url = `https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/products/${this.props.productID}/styles`;
-        axios(options)
-          .then((res) => {
-            buildItem.url = res.data.results[0].photos[0].url;
-            options.url = `https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/reviews/`;
-            axios(options)
-              .then((res) => {
-                var rating = 0;
+          options.url = `https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/products/${this.props.productID}/styles`;
+          axios(options)
+            .then((res) => {
+              buildItem.url = res.data.results[0].photos[0].url;
+              options.url = `https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/reviews/`;
+              axios(options)
+                .then((res) => {
+                  var rating = 0;
 
-                for (var x = 0; x < res.data.results.length; x++) {
-                  rating += res.data.results[x].rating;
-                }
+                  for (var x = 0; x < res.data.results.length; x++) {
+                    rating += res.data.results[x].rating;
+                  }
 
-                buildItem.rating = rating / res.data.results.length;
-                this.setState({ cardData: buildItem });
-              })
-              .catch((err) => {
-                console.log(err);
-              })
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+                  buildItem.rating = rating / res.data.results.length;
+                  this.setState({ cardData: buildItem });
+                })
+                .catch((err) => {
+                  console.log(err);
+                })
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
   }
   eventHandler(e) {
@@ -73,22 +74,27 @@ class RelatedProductCard extends React.Component {
   }
   render() {
     return (
-      <div className='singleCard' onClick={(e) => this.eventHandler(e.target.classList[0])}>
-        <div className='imageContainer'>
-          <img className='cardImage' src={this.state.cardData.url}></img>
-          <FaRegStar className='imageButton' />
+      <div>
+        {/* <Modal /> */}
+<div>
+        <div className='singleCard' onClick={(e) => this.eventHandler(e.target.classList[0])}>
+          <div className='imageContainer'>
+            <img className='cardImage' src={this.state.cardData.url}></img>
+            <FaRegStar className='imageButton' />
+          </div>
+          <div>
+            <p className='ProductInfo'>{this.state.cardData.category}</p>
+            <p className='ProductInfo'>{this.state.cardData.name}</p>
+            <p className='ProductInfo'>${this.state.cardData.price} </p>
+            <ReactStars
+              count={5}
+              isHalf={true}
+              value={this.state.cardData.rating}
+              activeColor="#ffd700"
+              className='ProductInfo'
+            />
+          </div>
         </div>
-        <div>
-          <p className='ProductInfo'>{this.state.cardData.category}</p>
-          <p className='ProductInfo'>{this.state.cardData.name}</p>
-          <p className='ProductInfo'>${this.state.cardData.price} </p>
-          <ReactStars
-            count={5}
-            isHalf={true}
-            value={this.state.cardData.rating}
-            activeColor="#ffd700"
-            className='ProductInfo'
-          />
         </div>
       </div>
     );
